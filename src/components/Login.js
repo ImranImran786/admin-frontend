83e8a670f609ac856693f5b9670a58df43971530
ImranImran786 <168235032+ImranImran786@users.noreply.github.com>
2025-06-30 10:26:48 +0500

    six

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("https://mongo-db-backend-production.up.railway.app/api/auth/login", {
+      const { data } = await axios.post("https://database-production-3a68.up.railway.app/api/auth/login", {
         email,
         password,
       });
